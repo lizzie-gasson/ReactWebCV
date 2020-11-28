03a24781000ac6b4dd4c57739ea848fc01a94f14
Lizzie Gasson <lizzie.gasson@iag.co.nz>
2020-11-28 19:49:57 +1300

    Created project

--- src/index.js
+++ src/index.js
@@ -1,17 +1,17 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from 'react'
+import ReactDOM from 'react-dom'
 
+
+//create react component
+const App = function(){
+    return <div>Lizzie's page!</div>
+};
+//Should I use function or class based components??
+//todo set up git repo
+
+
+//show component on the screen
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+    <App/>,
+    document.querySelector('#root')
 );
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
