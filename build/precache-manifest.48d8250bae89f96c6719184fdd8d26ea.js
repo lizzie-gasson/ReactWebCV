ec739e8c53bf605180f4a1032b36d2c34cc9f5e4
Liz454 <lizzie.gasson@gmail.com>
2020-12-02 23:03:46 +1300

    adjusted viewport for mobile, and tracked build folder

---
+++ build/precache-manifest.48d8250bae89f96c6719184fdd8d26ea.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6969621c932c74185000575576ed644e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3ac6409863ed6a644e21",
+    "url": "/static/css/main.0ed8f187.chunk.css"
+  },
+  {
+    "revision": "d46a9262f950dcfe4f25",
+    "url": "/static/js/2.6023da87.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.6023da87.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3ac6409863ed6a644e21",
+    "url": "/static/js/main.3d25c073.chunk.js"
+  },
+  {
+    "revision": "5a92be44ac184666d294",
+    "url": "/static/js/runtime-main.cc60cf41.js"
+  },
+  {
+    "revision": "475fe0b85c74922cc7673f9b77ee7aff",
+    "url": "/static/media/codepen-white.475fe0b8.svg"
+  },
+  {
+    "revision": "b51bca46d175510485477466fa1d87a6",
+    "url": "/static/media/envelope.b51bca46.svg"
+  },
+  {
+    "revision": "77a602c85d1c3dd4d101db95bbd08be1",
+    "url": "/static/media/github-white.77a602c8.svg"
+  },
+  {
+    "revision": "adc5e544e13f41c21df68d164f8d52bf",
+    "url": "/static/media/linkedin-white.adc5e544.svg"
+  }
+]);
