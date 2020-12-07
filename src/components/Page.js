58494c495afd434db6d57999e256630e2e4470ec
Liz454 <lizzie.gasson@gmail.com>
2020-12-07 17:32:11 +1300

    mobile sizing fix

--- src/components/Page.js
+++ src/components/Page.js
@@ -27,6 +27,8 @@ class Page extends React.Component {
 
     // }
 
+    
+
     scrollToView = () => {
         ReactDOM.findDOMNode(this).scrollIntoView();
         console.log(ReactDOM.findDOMNode(this).getBoundingClientRect().left);
@@ -36,6 +38,11 @@ class Page extends React.Component {
 
     componentDidMount() {
         document.addEventListener('click', this.getPageInView);
+
+        // First we get the viewport height and we multiple it by 1% to get a value for a vh unit
+        const vh = window.innerHeight * 0.01;
+        // Then we set the value in the --vh custom property to the root of the document
+        document.documentElement.style.setProperty('--vh', `${vh}px`);
     }
 
     renderSwitch(param) {
