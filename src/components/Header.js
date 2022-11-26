e1a1c767148f2fc9627d2a6a4a7be3ff52fbdb2d
Liz454 <lizzie.gasson@gmail.com>
2022-11-26 16:25:28 +1100

    Update github link and expo qr code

--- src/components/Header.js
+++ src/components/Header.js
@@ -15,7 +15,7 @@ class Header extends React.Component {
                 <div className="contact-links">
                     <a target="_blank" rel="noopener noreferrer" className="link email" href="mailto:lizzie.gasson@gmail.com"></a>
                     <a target="_blank" rel="noopener noreferrer" className="link codepen" href="https://codepen.io/LizzieGasson/"></a>
-                    <a target="_blank" rel="noopener noreferrer" className="link github" href="https://github.com/Liz454"></a>
+                    <a target="_blank" rel="noopener noreferrer" className="link github" href="https://github.com/lizzie-gasson"></a>
                     <a target="_blank" rel="noopener noreferrer" className="link linkedin" href="https://www.linkedin.com/in/lizziegasson/"></a>
                 </div>
             </div>
