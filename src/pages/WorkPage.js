c7b31c28d843c826435f9e39e44b4a0082b81505
Liz454 <lizzie.gasson@gmail.com>
2022-11-26 18:17:54 +1100

    update work experience page

--- src/pages/WorkPage.js
+++ src/pages/WorkPage.js
@@ -8,23 +8,27 @@ class WorkPage extends React.Component {
                 <h1>Work and Experience</h1>
 
                 <div className="work-element">
-                    <h2>Developer - IAG</h2>
-                    <h3>July 2018 - Now</h3>
-                    <p>I started working at IAG as a graduate during my final semester of study. I work on the quote and buy portals now, primarily doing front end work but as a team we all work on different areas including the back end, analytics and databases. While my scrum team worked on the Home and Contents websites during the year of 2020, sales increased by 72%, with an increase in conversion rate of 100% for the State house site, and an increase in conversion rate of 200% for the AMI house site. Throughout my time working at IAG I have done a re-skin on each of the 4 projects that my team worked on (home, contents and car for both the AMI and State brands). We also work with analytics systems such as Tealeaf, VWO, and Grafana.</p><p>
-At IAG I was part of the social club during 2019 where we planned events and activities for the digital team. I also worked with new graduates and starters helping them get up to speed.</p>
-                </div>
-                
-                <div className="work-element">
-                    <h2>Content creator - Daikin</h2>
-                    <h3>Aug 2020 - Oct 2020</h3>
-                    <p>I worked with Daikin on their Healthy Homes competition to document the process of the home makeover, and tell the story of the winner. I took photos and filmed throughout the process, using adobe premiere and audition to put the final products together.</p>
+                    <h2>Full Stack Engineer - Milanote</h2>
+                    <h3>November 2022 - Now</h3>
+                    <p>I am currently working on the <a style={{color: "white"}} target="_blank" href='https://milanote.com/'>Milanote web app</a> as a full stack engineer. Here I am using React and javascript day to day, and I'm excited to be working on such a visual tool.</p>
                 </div>
 
                 <div className="work-element">
-                    <h2>Content creator - Middlemore Foundation</h2>
-                    <h3>Feb 2019</h3>
-                    <p>I worked with the Middlemore foundation creating video content to be used in social media. I worked with a colleague to create a set of videos that told the story of a girl that had to go through years of hospital treatment.</p>
-                    
+                    <h2>Developer II - IAG AU</h2>
+                    <h3>April 2021 - November 2022</h3>
+                    <p>In the first scrum team I spent lots of time working with databases using Microsoft SQL Server. In this team I had a large input in changing the processes to improve organisation and productivity, using my experience from other agile scrum teams. This generated good feedback from outside the team. I also put a big focus on creating good documentation during this time.In the second team I was working on a greenfield project using React, Typescript and Redux. In this team I improved my technical skills and had lots of input and discussion around the setup of the project, the accompanying BFF project, as well as setting up and maintaining thelogging which we use to solve issues during work and when doing production support.</p>
+                </div>
+
+                <div className="work-element">
+                    <h2>Developer I - IAG NZ</h2>
+                    <h3>July 2018 - April 2021</h3>
+                    <p>I started working at IAG as a graduate during my final semester of study. At IAG in New Zealand I spent my time in scrum teams working on quote and buy portals, primarily front end work but I also had some involvement with analytics and back end code. During my time here I did a reskin of all the projects we worked on (home, contents and car for both the AMI and State brands) as I have strong css and styling skills. We worked with analytics systems such as Tealeaf and VWO, plus Prometheus and Grafana for logging. We had a regular formal sprintreview meeting where I would present the work that I or the team had done to a number of teammates and stakeholders. At IAG I was part of the social club during 2019 where we planned events and activities for the digital team. I also worked with new graduates and starters helping them get up to speed.</p>
+                </div>
+                
+                <div className="work-element">
+                    <h2>Content creator</h2>
+                    <h3>2019 - 2020</h3>
+                    <p>After working as a photographer for many years, I used my skills to do a few jobs creating video and photo content for different companies. In 2020 I worked with Daikin on their healthy homes competition to document the process of the home makeover, and to tell the story of the winning family. I took photos and filmed the process, using adobe premiere and audition to put the final products together. In 2019 I worked with the Middlemore Foundation creating photo and video content to be used in social media. I worked with a collegue to create a set of videos that told the story of a girl who had to go through years of hospital treatment.</p>
                 </div>
                 
                 <div className="work-element">
